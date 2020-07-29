5e6302e6902c0af581acba8aa6a4c7fd540760cf
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-07-29 10:59:30 +0200

    removed trailing spaces

--- commons/Movie.java
+++ commons/Movie.java
@@ -1,32 +1,32 @@
-/* 
+/*
  * Copyright (C) 2020 - Angelo Di Iorio
- * 
+ *
  * Progetto Movida.
  * Corso di Algoritmi e Strutture Dati
  * Laurea in Informatica, UniBO, a.a. 2019/2020
- * 
+ *
 */
 package movida.commons;
 
 /**
  * Classe usata per rappresentare un film
  * nell'applicazione Movida.
- * 
- * Un film è identificato in modo univoco dal titolo 
- * case-insensitive, senza spazi iniziali e finali, senza spazi doppi. 
- * 
- * La classe può essere modicata o estesa ma deve implementare tutti i metodi getter
+ *
+ * Un film ÔøΩ identificato in modo univoco dal titolo
+ * case-insensitive, senza spazi iniziali e finali, senza spazi doppi.
+ *
+ * La classe puÔøΩ essere modicata o estesa ma deve implementare tutti i metodi getter
  * per recupare le informazioni caratterizzanti di un film.
- * 
+ *
  */
 public class Movie {
-	
+
 	private String title;
 	private Integer year;
 	private Integer votes;
 	private Person[] cast;
 	private Person director;
-	
+
 	public Movie(String title, Integer year, Integer votes,
 			Person[] cast, Person director) {
 		this.title = title;
@@ -55,6 +55,6 @@ public class Movie {
 	public Person getDirector() {
 		return this.director;
 	}
-	
-	
+
+
 }
