0993369eef045996b42b3120944eeb1f3a0f2669
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-07-30 12:18:03 +0200

    implementato setSort

--- src/movida/fabbridonno/MovidaCore.java
+++ src/movida/fabbridonno/MovidaCore.java
@@ -1,5 +1,23 @@
 package movida.fabbridonno;
 import movida.commons.*;
 public class MovidaCore implements IMovidaConfig,IMovidaDB,IMovidaSearch,IMovidaCollaborations {
-    
+
+    private final SortingAlgorithm SAs[] = {SortingAlgorithm.InsertionSort, SortingAlgorithm.QuickSort};
+    private SortingAlgorithm selectedSort;
+
+    public MovidaCore()
+    {
+        selectedSort = null;
+    }
+
+    public boolean setSort(SortingAlgorithm a)
+    {
+        if(selectedSort != a && (a == SAs[0] || a == SAs[1]))
+        {
+            selectedSort = a;
+            return true;
+        }
+
+        return false;
+    }
 }
