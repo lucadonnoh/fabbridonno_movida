b2d14c6a099f338b1354cbbc4250de4604f91bcb
Yureehh <fabbri.juri@gmail.com>
2020-07-30 12:58:54 +0200

    implementata funzione setMap

--- src/movida/fabbridonno/MovidaCore.java
+++ src/movida/fabbridonno/MovidaCore.java
@@ -1,13 +1,16 @@
 package movida.fabbridonno;
 import movida.commons.*;
-public class MovidaCore implements IMovidaConfig,IMovidaDB,IMovidaSearch,IMovidaCollaborations {
+public class MovidaCore implements IMovidaConfig {
 
     private final SortingAlgorithm SAs[] = {SortingAlgorithm.InsertionSort, SortingAlgorithm.QuickSort};
     private SortingAlgorithm selectedSort;
+    private final MapImplementation Maps[] = {MapImplementation.ListaNonOrdinata, MapImplementation.HashIndirizzamentoAperto};
+    private MapImplementation selectedMap;
 
     public MovidaCore()
     {
         selectedSort = null;
+        selectedMap  = null;
     }
 
     /**
@@ -32,9 +35,26 @@ public class MovidaCore implements IMovidaConfig,IMovidaDB,IMovidaSearch,IMovida
         return false;
     }
 
+    public boolean setMap(MapImplementation m)
+    {
+        if(selectedMap != m)
+        {
+            for(MapImplementation SM : Maps )
+            {
+                if(m == SM)
+                {
+                    selectedMap = m;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         MovidaCore test = new MovidaCore();
-        boolean b = test.setSort(SortingAlgorithm.MergeSort);
+        boolean b = test.setMap(MapImplementation.BTree);
+        b = test.setMap(MapImplementation.ListaNonOrdinata);
         System.out.println(b);
     }
 }
