af729611648d7d2660a79c4e91c4692613455f8d
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-07-31 11:50:15 +0200

    classe tabellahash

---
+++ src/movida/fabbridonno/TabellaHashAperta.java
@@ -0,0 +1,31 @@
+package movida.fabbridonno;
+import movida.commons.*;
+
+public class TabellaHashAperta {
+    protected Movie[] v;
+
+    public TabellaHashAperta()
+    {
+
+    }
+
+    public void insert()
+    {
+
+    }
+
+    public void delete()
+    {
+
+    }
+
+    // public Movie search()
+    // {
+
+    // }
+
+    // protected int indice()
+    // {
+
+    // }
+}
