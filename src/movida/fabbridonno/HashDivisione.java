a89f0770a5986c370f50c3c536d224f5b069906c
lucadonnoh <donnoh99@gmail.com>
2020-12-22 11:57:03 +0100

    un po di hash

--- src/movida/fabbridonno/HashDivisione.java
+++
@@ -1,8 +0,0 @@
-package movida.fabbridonno;
-
-public class HashDivisione {
-
-    public int h(Object k, int m) {
-		return Math.abs(k.hashCode()) % m;
-    }
-}
--- src/movida/fabbridonno/TabellaHashAperta.java
+++ src/movida/fabbridonno/TabellaHashAperta.java
@@ -1,86 +1,115 @@
 package movida.fabbridonno;
+
 import movida.commons.*;
 
-public class TabellaHashAperta<T, K extends Comparable<K>> implements DizionarioFilm<T, K>{
+public class TabellaHashAperta<T, K extends Comparable<K>> implements DizionarioFilm<T, K> {
     protected Movie[] v;
 
-    public TabellaHashAperta()
+    private int carico;
+    private Record<T, K>[] array;
+
+    private Record<T, K> DELETED;
+
+    @SuppressWarnings("unchecked")
+    public TabellaHashAperta() {
+        carico = 0;
+        array = new Record[1];
+    }
+
+    public int hash(Object k, int m) {
+        return Math.abs(k.hashCode()) % m;
+    }
+
+    public int ispezione(int i, int hk, int m) {
+        double c1 = 0.5, c2 = 0.5;
+        return (int) Math.floor(hk + c1 * i + c2 * i * i) % m;
+    }
+
+    private void reshape()
     {
 
     }
 
-    public void insert(T m, K k)
-    {
+    public void insert(T m, K k) {
+        int i = 0;
+        if(carico == array.length) {
+            reshape();
+        }
+        // a questo punto c'è almeno uno spazio libero
+        while (true) {
+            int h = ispezione(i++, hash(k, array.length), array.length);
+            if (array[h] == null || array[h] == DELETED) {
+                array[h] = new Record<T, K>(m, k);
+                return;
+            }
+        }
 
     }
 
-    public boolean delete(K k)
-    {
+    public boolean delete(K k) {
         return false;
     }
 
-    public T search(K k)
-    {
-        return null;
+    public T search(K k) {
+        int i = 0;
+        while(i<array.length) {
+            int h = ispezione(i++, hash(k, array.length), array.length);
+            if(array[h].getKey().equals(k)) {
+                return array[h].getEl();
+            }
+        }
+        return null; // TODO: lanciare eccezione?
     }
 
-    public void stampa()
-    {
+    public void stampa() {
         return;
     }
 
-    public Movie[] export()
-    {
+    public Movie[] export() {
         return null;
     }
 
-    public Comparable<K>[] exportKeys()
-    {
+    public Comparable<K>[] exportKeys() {
         return null;
     }
 
-    public Boolean searchKey(K k)
-    {
+    public Boolean searchKey(K k) {
         return null;
     }
 
-
-    public int getCarico(){
+    public int getCarico() {
         return -1;
     }
 
-    public void clear(){
+    public void clear() {
 
     }
 
-    public Movie[] searchMoviesByKey(K k)
-    {
+    public Movie[] searchMoviesByKey(K k) {
         return null;
     }
 
-    public Movie[] firstNMovies(int n){
+    public Movie[] firstNMovies(int n) {
         return null;
     }
 
-    public Person[] firstNActors (int n){
+    public Person[] firstNActors(int n) {
         return null;
     }
 
-    public Movie[] stringInTitle(String title){
+    public Movie[] stringInTitle(String title) {
         return null;
     }
 
-    public void sort(int index, boolean b)
-    {
+    public void sort(int index, boolean b) {
 
     }
 
-    public Record<T, K> searchRecord(K k)
-    {
+    public Record<T, K> searchRecord(K k) {
         return null;
     }
 
-    public void insertionSort(boolean b){
+    public void insertionSort(boolean b) {
         return;
     }
 }
